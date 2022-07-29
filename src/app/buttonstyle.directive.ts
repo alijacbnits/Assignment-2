09f3fb114499c89a786211e42d37cc79b5c94cf0
Your Name <you@example.com>
2022-07-29 14:54:29 +0530

    Button component format done

--- src/app/buttonstyle.directive.ts
+++ src/app/buttonstyle.directive.ts
@@ -1,23 +1,19 @@
-import { Directive, ElementRef, HostListener  } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appButtonstyle]'
+  selector: '[appButtonstyle]',
 })
 export class ButtonstyleDirective {
+  constructor(private el: ElementRef) {}
+  ngOnInit() {}
 
-  constructor(private el: ElementRef) { }
-  ngOnInit() {
-
+  //for onclick function
+  @HostListener('click', ['$event'])
+  clickEvent(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.el.nativeElement.style.marginLeft = '45%';
+    this.el.nativeElement.style.backgroundColor = 'black';
+    this.el.nativeElement.innerText = 'Cancel';
   }
-
-   //for onclick function
-   @HostListener('click', ['$event'])
-   clickEvent(event:any) {
-     event.preventDefault();
-     event.stopPropagation();
-     this.el.nativeElement.style.marginLeft="45%";
-     this.el.nativeElement.style.backgroundColor="black";
-     this.el.nativeElement.innerText="Cancel";
-   }
-
 }
